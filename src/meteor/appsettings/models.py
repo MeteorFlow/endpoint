558f8a6294865c63ef22f2ef5793c119a49b0e3b
TuanKietTran <kyletran101.work@gmail.com>
2023-12-26 21:40:48 +0700

    Init REST API

---
+++ src/meteor/appsettings/models.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from sqlalchemy import Column, String
+from meteor.database import Base
+
+from meteor.models import UUIDMixin
+
+class AppSettingsEntity(Base):
+    key = Column(String(255))
+    value = Column(String(255))
+    description = Column(String(1023))
+    type = Column(String(255))
+
+class AppSettings(BaseModel, UUIDMixin):
+    key: str
+    value: str
+    description: str
+    type: str
---
+++ src/meteor/database/versions/825bfd249831_first_migrations.py
@@ -0,0 +1,26 @@
+"""first migrations
+
+Revision ID: 825bfd249831
+Revises: 
+Create Date: 2023-12-12 00:48:16.652487
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '825bfd249831'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
